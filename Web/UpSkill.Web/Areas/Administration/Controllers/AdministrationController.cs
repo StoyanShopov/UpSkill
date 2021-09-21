70b2d6617b06446fb5a0e80227b4c69bd84c46c1
ZdravkoBorisov <45686031+ZdravkoBorisov@users.noreply.github.com>
2021-09-21 11:57:17 +0300

    Initial Create
    Initial create of the back-end project template.

---
+++ Web/UpSkill.Web/Areas/Administration/Controllers/AdministrationController.cs
@@ -0,0 +1,16 @@
+﻿namespace UpSkill.Web.Areas.Administration.Controllers
+{
+    using UpSkill.Common;
+    using UpSkill.Web.Controllers;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+    [ApiController]
+    [Route("[controller]")]
+    [Area("Administration")]
+    public class AdministrationController : ControllerBase 
+    {
+    }
+}
---
+++ Web/UpSkill.Web/Controllers/IdentityController.cs
@@ -0,0 +1,47 @@
+﻿namespace UpSkill.Web.Controllers
+{
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    using System.Threading.Tasks;
+
+    using UpSkill.Services.Contracts.Identity;
+    using UpSkill.Web.ViewModels.Identity;
+
+    public class IdentityController : ApiController
+    {
+        private readonly IIdenityService identity; 
+
+        public IdentityController(IIdenityService identity) => this.identity = identity;
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route(nameof(Register))]
+        public async Task<IActionResult> Register(RegisterRequestModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            await this.identity.RegisterAsync(model);
+
+            return StatusCode(201);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route(nameof(Login))]
+        public async Task<ActionResult<LoginResponseModel>> Login(LoginRequestModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var token = await this.identity.LoginAsync(model);
+
+            return token;
+        }
+    }
+}
