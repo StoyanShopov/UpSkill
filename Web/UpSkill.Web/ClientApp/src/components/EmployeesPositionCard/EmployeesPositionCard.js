58737d48acdcc43a731a2138723a640f5bc5c448
Iliya Todorov <53480117+iltodbul@users.noreply.github.com>
2021-09-26 17:24:17 +0300

    Extract css in separate files

--- Web/UpSkill.Web/ClientApp/src/components/EmployeesPositionCard/EmployeesPositionCard.js
+++ Web/UpSkill.Web/ClientApp/src/components/EmployeesPositionCard/EmployeesPositionCard.js
@@ -1,31 +1,22 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
+import EmployeeRow from './EmployeeRow';
+import './EmployeesPositionCard.css';
 
 function EmployeesPositionCard() {
   return (
-    <Card style={{ width: '38rem' }}>
-      <Card.Header style={{ backgroundColor: '#296CFB', color: '#FFFFFF' }}>
-        <p>Employees(65) Position</p>
+    <Card className="card">
+      <Card.Header className="cardHeader">
+        <span id='employee'>Employees(65)</span> <span id='position'>Position</span>
       </Card.Header>
       <Card.Body className="text-center">
-        <Card.Text style={{ 'text-align': 'left', font: 'Montserrat' }}>
-          {Row()}
-          {Row()}
-          {Row()}
+        <Card.Text>
+          <EmployeeRow />
+          <EmployeeRow />
         </Card.Text>
         <Button variant="link">View More</Button>
       </Card.Body>
     </Card>
-  );
-}
-
-function Row() {
-  return (
-    <div>
-      <strong style={{ color: '#296CFB' }}>Lora Petrova</strong>{' '}
-      <span>Graphic Designer</span>
-      <hr />
-    </div>
   );
 }
 
--- Web/UpSkill.Web/ClientApp/src/components/Shared/Footer/Footer.js
+++ Web/UpSkill.Web/ClientApp/src/components/Shared/Footer/Footer.js
@@ -1,58 +1,109 @@
-﻿import { NavLink } from "react-router-dom";
-import React, { useState } from 'react'
+﻿import { NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
 
-import FooterLOGO from "../../../assets/footerLOGO-NoBg.png";
+import FooterLOGO from '../../../assets/footerLOGO-NoBg.png';
 import './Footer.css';
 
 function FooterMenu() {
-    return (
-            <footer>
-                <div className="slants">
-                </div>
-            <div className="content-wrapper container-fluid">
-                <div className="content container contentFooter">
-                    <div className="row">
-                        <div className="col-md-4">
-                            <h2><img src={FooterLOGO} alt="Footer Logo" className="w-25" /> upskill</h2>
-                            <p className="pr-5 pt-3 pb-3">Upskill gives everyone the opportunity to grow professionally and develop into specialist in every field.</p>
-                            <NavLink to="" className="btn btn-outline-light rounded-0 getStarted font-weight-bold pl-3 pr-3 pb-2 pt-2 fw-bolder">Get Started</NavLink>
-                        </div>
-                        <div className="col-md-3 mt-5 ml-auto">
-                            <ul className="nav flex-column">
-                                <h5 className="nav-brand pl-3 m-1">Company</h5>
-                                <li className="nav-item m-1"><NavLink to="" className="nav-link text-white">About us</NavLink></li>
-                                <li className="nav-item m-1"><NavLink to="" className="nav-link text-white">Contact us</NavLink></li>
-                                <li className="nav-item m-1"><NavLink to="" className="nav-link text-white">Privacy Policy</NavLink></li>
-                                </ul>
-                            </div>
-                        <div className="col-md-3 mt-5">
-                            <ul className="nav flex-column">
-                                <h5 className="nav-brand pl-3 m-1">Services</h5>
-                                <li className="nav-item m-1"><NavLink to="" className="nav-link text-white">Courses</NavLink></li>
-                                <li className="nav-item m-1"><NavLink to="" className="nav-link text-white">Coaches</NavLink></li>
-                                <li className="nav-item m-1"><NavLink to="" className="nav-link text-white">Requests</NavLink></li>
-                            </ul>
-                        </div>
-                        <div className="col-md-1 mt-5">
-                            <ul className="nav flex-column mt-3">
-                                <li className="nav-item m-1"><NavLink to="" className="nav-link text-white">F</NavLink></li>
-                                <li className="nav-item m-1"><NavLink to="" className="nav-link text-white">I</NavLink></li>
-                                <li className="nav-item m-1"><NavLink to="" className="nav-link text-white">In</NavLink></li>
-                            </ul>
-                        </div>
-                    </div>                    
-                </div>               
+  return (
+    <footer>
+      <div className="slants"></div>
+      <div className="content-wrapper container-fluid">
+        <div className="content container contentFooter">
+          <div className="row">
+            <div className="col-md-4">
+              <h2>
+                <img src={FooterLOGO} alt="Footer Logo" className="w-25" />{' '}
+                upskill
+              </h2>
+              <p className="pr-5 pt-3 pb-3">
+                Upskill gives everyone the opportunity to grow professionally
+                and develop into specialist in every field.
+              </p>
+              <NavLink
+                to=""
+                className="btn btn-outline-light rounded-0 getStarted font-weight-bold pl-3 pr-3 pb-2 pt-2 fw-bolder"
+              >
+                Get Started
+              </NavLink>
             </div>
+            <div className="col-md-3 mt-5 ml-auto">
+              <ul className="nav flex-column">
+                <h5 className="nav-brand pl-3 m-1">Company</h5>
+                <li className="nav-item m-1">
+                  <NavLink to="" className="nav-link text-white">
+                    About us
+                  </NavLink>
+                </li>
+                <li className="nav-item m-1">
+                  <NavLink to="" className="nav-link text-white">
+                    Contact us
+                  </NavLink>
+                </li>
+                <li className="nav-item m-1">
+                  <NavLink to="" className="nav-link text-white">
+                    Privacy Policy
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-3 mt-5">
+              <ul className="nav flex-column">
+                <h5 className="nav-brand pl-3 m-1">Services</h5>
+                <li className="nav-item m-1">
+                  <NavLink to="" className="nav-link text-white">
+                    Courses
+                  </NavLink>
+                </li>
+                <li className="nav-item m-1">
+                  <NavLink to="" className="nav-link text-white">
+                    Coaches
+                  </NavLink>
+                </li>
+                <li className="nav-item m-1">
+                  <NavLink to="" className="nav-link text-white">
+                    Requests
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-1 mt-5">
+              <ul className="nav flex-column mt-3">
+                <li className="nav-item m-1">
+                  <NavLink to="" className="nav-link text-white">
+                    F
+                  </NavLink>
+                </li>
+                <li className="nav-item m-1">
+                  <NavLink to="" className="nav-link text-white">
+                    I
+                  </NavLink>
+                </li>
+                <li className="nav-item m-1">
+                  <NavLink to="" className="nav-link text-white">
+                    In
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
 
-            <div id="CopyRigthFooter" className="mt-4 border-top-2 border-white content-wrapper container-fluid">
-                <div className="content container">
-                    <div className="row d-flex justify-content-center">
-                        <span className="col-md-4 text-center">&copy; UpSkill {(new Date().getFullYear())}</span>
-                        </div>
-                </div>
-            </div>
-        </footer>
-		);
+      <div
+        id="CopyRigthFooter"
+        className="mt-4 border-top-2 border-white content-wrapper container-fluid"
+      >
+        <div className="content container">
+          <div className="row d-flex justify-content-center">
+            <span className="col-md-4 text-center">
+              &copy; UpSkill {new Date().getFullYear()}
+            </span>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default FooterMenu;
