8ef95e9af0374ce634e76e90e165cf125345ca1d
Damyan Dimitrov <damqn_96@abv.bg>
2021-09-27 05:18:28 +0300

    Add CompanyOwner-Employees content
    Two table components with coresponding services

--- Web/UpSkill.Web/ClientApp/src/components/Coaches/Coaches.js
+++ Web/UpSkill.Web/ClientApp/src/components/Coaches/Coaches.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import CategoriesAndLanguageMenu from '../CategoriesAndLanguageMenu/categoryAndLanguageMenu';
-import CoachList from '../CompanyOwnerViews/CompanyCoaches/CompanyCoaches';
+import Employees from '../CompanyOwnerViews/Employees/Employees';
 
 import './Coaches.css';
 
 
 export default function Coaches() {
-    return (
-      <div className="content">
-            <CategoriesAndLanguageMenu atPage="Coaches"/>
-            < CoachList />
+  return (
+    <div className="content">
+      <CategoriesAndLanguageMenu atPage="Coaches" />
+      <div className="wrapper">
+
       </div>
-    );
-  }
+    </div>
+  );
+}
