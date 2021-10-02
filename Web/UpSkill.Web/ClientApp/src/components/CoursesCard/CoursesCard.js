6d1ddeef74d6ca4cd7c907c80b1a60399f8f51dd
Iliya Todorov <53480117+iltodbul@users.noreply.github.com>
2021-10-02 18:45:15 +0300

    Changes the coursesCard according to new design

--- Web/UpSkill.Web/ClientApp/src/components/CoursesCard/CoursesCard.js
+++ Web/UpSkill.Web/ClientApp/src/components/CoursesCard/CoursesCard.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import './CoursesCard.css';
 
 function CoursesCard() {
   return (
-    <Card className="cardContainer">
-      <Card.Header className="cardColors">HTML & CSS</Card.Header>
-      <Card.Body>
-        <Card.Title className="cardText">What you’ll learn:</Card.Title>
-        <Card.Text className="cardText">
-          This course includes studies of various literary genres: short story,
-          poetry, novel, drama, fiction and non-fiction.
-        </Card.Text>
-        <Button className="cardColors">Compete</Button>
-      </Card.Body>
-    </Card>
+    <div className="cardContainer">
+      <div className="background"></div>
+      <div className="cardText">
+        <span id='course'>Marketing</span> <span id='name'>Jim Wilber</span>
+      </div>
+      <div></div>
+      <Button className="cardColors">Enroll</Button>
+    </div>
   );
 }
 
