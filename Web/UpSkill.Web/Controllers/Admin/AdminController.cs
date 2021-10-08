4b4b25acd98b522c339903a0747f3b6c96bf4cb4
Chris Ivanov <65026817+ChrisIvanov@users.noreply.github.com>
2021-10-08 08:38:56 +0300

    Development Upload and Updated Admin Controller

---
+++ Web/UpSkill.Web/Controllers/Admin/AdminController.cs
@@ -0,0 +1,69 @@
+﻿namespace UpSkill.Web.Controllers.SuperAdminController
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using UpSkill.Services.Data.Admin;
+    using UpSkill.Web.ViewModels.Administration.Courses;
+
+    using static Common.GlobalConstants;
+
+    public class CoursesController : Controller
+    {
+        private readonly Repository repository;
+
+        public CoursesController(Repository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<ActionResult> CreateCourse(CourseFormModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ErrorMessage);
+            }
+
+            await this.repository.CreateCourse(model);
+
+            return Ok(SuccessMessage);
+        }
+
+        public async Task<ActionResult> GetCourseById(string id)
+        {
+            var result = await this.repository.GetCourseById(id);
+
+            if (result == null)
+            {
+                return BadRequest(ErrorMessage);
+            }
+
+            return Ok(result);
+        }
+
+        public async Task<ActionResult> EditCourse(CourseFormModel model, string id)
+        {
+            var result = await this.repository.EditCourse(model, id);
+
+            if (result == null)
+            {
+                return BadRequest(ErrorMessage);
+            }
+
+            return Ok(SuccessMessage);
+        }
+
+        public async Task<ActionResult> DeleteCourse(string id)
+        {
+            var result = await this.repository.DeleteCourse(id);
+
+            if (result == null)
+            {
+                return BadRequest(ErrorMessage);
+            }
+
+            return Ok(SuccessMessage);
+        }
+    }
+}
---
+++ Web/UpSkill.Web/Controllers/IdentityController.cs
@@ -0,0 +1,126 @@
+﻿namespace UpSkill.Web.Controllers
+{
+    using System.Threading.Tasks;
+    using System.Security.Claims;
+
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.EntityFrameworkCore;
+
+    using UpSkill.Data.Models;
+    using UpSkill.Services.Contracts.Identity;
+    using UpSkill.Web.ViewModels.Identity;
+    using UpSkill.Services.Contracts.Email;
+
+    using static Common.GlobalConstants.IdentityConstants;
+    using static Common.GlobalConstants.ControllerRoutesConstants;
+    using static Common.GlobalConstants.MessagesConstants;
+
+    public class IdentityController : ApiController
+    {
+        private readonly IIdentityService identity;
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly IEmailService emailService;
+
+        public IdentityController(
+            IIdentityService identity,
+            UserManager<ApplicationUser> userManager,
+            IEmailService emailService)
+        {
+            this.identity = identity;
+            this.userManager = userManager;
+            this.emailService = emailService;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route(RegisterRoute)]
+        public async Task<IActionResult> Register(RegisterRequestModel model)
+        {
+            await ValidateRegisterModel(model);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var isUserRegistered = await this.identity.RegisterAsync(model);
+
+            if (isUserRegistered.Failure)
+            {
+                return BadRequest(isUserRegistered.Error);
+            } 
+
+            await EmailConfirmation(model.Email);  
+
+            return StatusCode(201);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route(LoginRoute)]
+        public async Task<ActionResult<LoginResponseModel>> Login(LoginRequestModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var embededToken = await this.identity.LoginAsync(model);
+
+            Response.Cookies.Append(JWT, embededToken.Token, new CookieOptions()
+            {
+                HttpOnly = true
+            });
+
+            return Ok(embededToken);
+        }
+
+        [HttpPost]
+        [Route(LogoutRoute)]
+        public IActionResult Logout()
+        {
+            Response.Cookies.Delete(JWT);
+
+            return Ok(new { message = SuccessMessage });
+        }
+
+        [HttpGet]
+        [Route(UserRoute)]
+        public async Task<LoginResponseModel> GetCurrentUser()
+        {
+            var user = await userManager.FindByEmailAsync(User.FindFirstValue(ClaimTypes.Email));
+
+            return new LoginResponseModel
+            {
+                Id = user.Id,
+                Email = user.Email,
+            };
+        }
+
+        private async Task EmailConfirmation(string email)
+        {
+            var user = await userManager.FindByEmailAsync(email);
+
+            var origin = Request.Headers[HeaderOrigin];
+            var host = Request.Host.Value;
+
+            await this.emailService.SendEmailConfirmationAsync(origin, host, user);
+        }
+
+        private async Task ValidateRegisterModel(RegisterRequestModel model)
+        {
+            if (await this.userManager.Users.AnyAsync(x => x.Email == model.Email))
+            {
+                ModelState.AddModelError(nameof(model.Email), EmailExist);
+            }
+
+            if (model.Password != model.ConfirmPassword)
+            {
+                ModelState.AddModelError(nameof(model.Password), PasswordNotMatch);
+            }
+        }
+    }
+}
