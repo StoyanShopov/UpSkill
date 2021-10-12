e7d2af3355c365d7a8ddecd4b9d05eff8101ea39
Damyan Dimitrov <damqn_96@abv.bg>
2021-10-12 06:51:29 +0300

    Update to last version of FE

--- Web/UpSkill.Web/ClientApp/src/components/Courses/CoursesCatalog/CoursesCard/CoursesCard.js
+++ Web/UpSkill.Web/ClientApp/src/components/Courses/CoursesCatalog/CoursesCard/CoursesCard.js
@@ -1,20 +1,36 @@
-import React from 'react';
-import { Card, Button } from 'react-bootstrap';
+
+import React, { useState, useEffect } from 'react';
+import { Button } from 'react-bootstrap';
+
 import './CoursesCard.css';
 
-
 function CoursesCard(props) {
+  const { courseName, coachName, imageName } = props;
+
+  const [Image, setImage] = useState();
+
+  function loadImage(imgName) {
+    import(`../../../../assets/img/courses/${imageName}`).then((img) =>
+      setImage(img.default)
+    );
+  }
+
+  useEffect(() => {
+    loadImage(imageName);
+  }, []);
+
   return (
     <div className="cardContainer">
-    <div className="background"></div>
-    <div className="cardText">
-      <span id='course'>Marketing</span> <span id='name'>Jim Wilber</span>
+      <div className="background">
+        <img src={Image} alt="courses" />
+      </div>
+      <div className="cardText">
+        <span id="course">{courseName}</span> <span id="name">{coachName}</span>
+      </div>
+      <div></div>
+      <Button className="cardColors">Enroll</Button>
     </div>
-    <div></div>
-    <Button className="cardColors">Enroll</Button>
-  </div>    
-);
+  );
 }
-
 
 export default CoursesCard;
