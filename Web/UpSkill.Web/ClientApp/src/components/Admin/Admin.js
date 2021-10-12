42e82216155d42abf4b98b3bc7ecffd41d7ff522
Kiril Nikov <kiril24us@yahoo.com>
2021-10-12 09:39:11 +0300

    remove one new line

--- Web/UpSkill.Web/ClientApp/src/components/Admin/Admin.js
+++ Web/UpSkill.Web/ClientApp/src/components/Admin/Admin.js
@@ -3,11 +3,10 @@ import Companies from "./Companies/Companies"
 import Employees from "./Employees/Employees"
 
 export default function Admin() {
-  return (
-    
-      <ul className="list-group">
+  return (     
+        <ul className="list-group">
         <li className="list-group-item"><Courses /></li>
-        <li className="list-group-item"><Companies /></li>
+        <li className="list-group-item"><Companies /></li>        
         <li className="list-group-item"><Employees /></li>
         <li className="list-group-item bold-element">To Do Revenue</li>
       </ul>   
