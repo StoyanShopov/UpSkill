02701043bad479575907471aee68486516fdff63
ZdravkoBorisov <45686031+ZdravkoBorisov@users.noreply.github.com>
2021-10-14 14:34:44 +0300

    Add Coach Table

---
+++ Data/UpSkill.Data.Models/Coach.cs
@@ -0,0 +1,20 @@
+﻿namespace UpSkill.Data.Models
+{
+    using System.Collections.Generic;
+
+    using Common.Models;
+
+    public class Coach : BaseDeletableModel<int>
+    {
+        public Coach()
+        {
+            this.Courses = new HashSet<Course>();
+        }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public ICollection<Course> Courses { get; set; }
+    }
+}
