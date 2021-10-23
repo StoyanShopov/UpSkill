2a3b8cc89b56a145475edbbb64db381b77dc5956
stosho <stosho@cobuilder.no>
2021-10-23 17:09:53 +0300

    test

--- Web/UpSkill.Web/Startup.cs
+++ Web/UpSkill.Web/Startup.cs
@@ -66,7 +66,7 @@
                     new ApplicationDbContextSeeder().SeedAsync(dbContext, serviceScope.ServiceProvider).GetAwaiter().GetResult();
                 }
 
-                if (env.IsDevelopment())
+                if (env.IsProduction())
                 {
                     app.UseDeveloperExceptionPage();
                     app.UseMigrationsEndPoint();
