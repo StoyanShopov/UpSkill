d6e8da15ba2374a4d3f895c039049358598ae5e4
Iliya Todorov <53480117+iltodbul@users.noreply.github.com>
2021-10-27 15:40:55 +0300

    Refactoring

--- Web/UpSkill.Web/ClientApp/src/services/companyService.js
+++ Web/UpSkill.Web/ClientApp/src/services/companyService.js
@@ -1,63 +1,69 @@
-
 import axios from 'axios';
 
 //RetriveCompany
 export const retriveCompanies = async () => {
-  const response = await axios
-  .get("https://localhost:44319/Admin/Companies/getAll");
+  const response = await axios.get(
+    'https://localhost:44319/Admin/Companies/getAll'
+  );
   return response.data;
-}
+};
+
 //Create
 export const addCompanyHandler = async (company) => {
-
   const request = {
     name: company.name,
   };
 
   return await axios
-  .post("https://localhost:44319/Admin/Companies/create", request)
-  .then(response => response.data);
+    .post('https://localhost:44319/Admin/Companies/create', request)
+    .then((response) => response.data);
 };
+
 //Update
 export const updateCompanyHandler = async (company) => {
   return await axios
-  .put(`https://localhost:44319/Admin/Companies/edit?id=${company.id}`, company)
-  .then(response => response.data);
+    .put(
+      `https://localhost:44319/Admin/Companies/edit?id=${company.id}`,
+      company
+    )
+    .then((response) => response.data);
 };
+
 //Delete
 export const removeCompanyHandler = async (id) => {
-  return await axios.delete(`https://localhost:44319/Admin/Companies/delete?id=${id}`);
+  return await axios.delete(
+    `https://localhost:44319/Admin/Companies/delete?id=${id}`
+  );
+};
 
 const initialCompanies = [
   {
     id: 1,
-    companyName: "Motion Software",
+    companyName: 'Motion Software',
   },
   {
     id: 2,
-    companyName: "Scale Focus",
+    companyName: 'Scale Focus',
   },
   {
     id: 3,
-    companyName: "SoftUni",
+    companyName: 'SoftUni',
   },
   {
     id: 4,
-    companyName: "Test",
+    companyName: 'Test',
   },
   {
     id: 5,
-    companyName: "Metro",
+    companyName: 'Metro',
   },
   {
     id: 6,
-    companyName: "Fantastiko",
+    companyName: 'Fantastiko',
   },
 ];
 
 export const getCompanies = async () => {
   //      let res = await request(``, 'Get');
-
   return initialCompanies;
-
 };
