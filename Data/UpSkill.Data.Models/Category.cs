7bb17683c8e4290d1b4dadaa39bc18abcb77c43f
Chris Ivanov <65026817+ChrisIvanov@users.noreply.github.com>
2021-11-05 15:30:41 +0200

    Delete Data directory

--- Data/UpSkill.Data.Models/Category.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UpSkill.Data.Models
-{
-    using UpSkill.Data.Common.Models;
-
-    public class Category : BaseDeletableModel<int>
-    {
-        public string Name { get; set; }
-    }
-}
