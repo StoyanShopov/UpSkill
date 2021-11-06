61e628371e7c3f537ff0c602a07f6a92b72b114b
Kiril Nikov <kiril24us@yahoo.com>
2021-11-06 09:56:02 +0200

    add package json files

---
+++ Web/UpSkill.Web/ClientApp/src/services/test.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const testGet = async () => {
+  try {
+    const resp = await axios.get("https://localhost:44319/Admin/Dashboard");
+    return resp;
+  } catch (err) {}
+};
