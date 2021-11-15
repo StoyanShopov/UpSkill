c5ee4ab5a86d8aee87ea430f25624ef842a6925a
Ivailo Topchiev <ivo_topchiev369@abv.bg>
2021-11-15 15:09:54 +0200

    Refactoring

---
+++ Web/UpSkill.Web/Pages/HomeModel.cshtml.cs
@@ -0,0 +1,11 @@
+﻿namespace UpSkill.Web.Pages
+{
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    public class HomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
