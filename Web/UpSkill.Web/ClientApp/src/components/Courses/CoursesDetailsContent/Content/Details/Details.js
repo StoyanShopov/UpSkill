da5d8bacc0762b99cce12b14d4214bf1e210a261
Ivailo Topchiev <ivo_topchiev369@abv.bg>
2021-11-16 11:49:13 +0200

    Apply changes

--- Web/UpSkill.Web/ClientApp/src/components/Courses/CoursesDetailsContent/Content/Details/Details.js
+++ Web/UpSkill.Web/ClientApp/src/components/Courses/CoursesDetailsContent/Content/Details/Details.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { ReactVideo } from 'reactjs-media';
 import { Editor } from '@tinymce/tinymce-react';
 
@@ -11,6 +11,8 @@ const Details = (props) => {
         courseDetails: { courseTitle, courseVideo, courseLecturer, courseDescription },
     } = props;
 
+    const [text, setText] = useState(courseDescription);
+
     return(
         <div className="container">
                 <h2 className="courseTitleContent">{courseTitle}</h2>
@@ -21,10 +23,29 @@ const Details = (props) => {
                 primaryColor="red"
                 /><br/>
                 <h4 className="lectureDescriptionContent">Lecture Description</h4>
-                <p className="descriptionContent">{courseDescription}</p>
+                <Editor
+                initialValue={text}
+                onEditorChange={(newText) => setText(newText)}
+                init={{
+                    height: 180,
+                    width: 700,
+                    plugins: [
+                        'advlist autolink lists link image charmap print preview anchor',
+                        'searchreplace visualblocks code fullscreen',
+                        'insertdatetime media table paste code help wordcount'
+                      ],
+                      toolbar: 'undo redo | formatselect | ' +
+                      'bold italic backcolor | alignleft aligncenter ' +
+                      'alignright alignjustify | bullist numlist outdent indent | ' +
+                      'removeformat | help',
+                      content_style: 'font: normal normal bold 22px/27px Montserrat;' + 
+                      'letter-spacing: 1.1px; color: #000000; opacity: 1;'
+                }}
+                /><br/>    
+                {/* <p className="descriptionContent">{courseDescription}</p> */}
                 <h4 className="instructorContent">Instructor</h4>
-                <p>{courseLecturer}</p>            
-        </div>    
+                <p>{courseLecturer}</p>                
+        </div>   
     )
 }
 
