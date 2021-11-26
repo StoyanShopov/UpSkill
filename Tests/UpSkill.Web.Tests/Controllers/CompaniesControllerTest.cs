d4a80fab85268028f06fd018b0d10c3167b68d38
Ivailo Topchiev <ivo_topchiev369@abv.bg>
2021-11-26 15:50:11 +0200

    Initial commit

---
+++ Tests/UpSkill.Web.Tests/Controllers/CompaniesControllerTest.cs
@@ -0,0 +1,24 @@
+﻿namespace UpSkill.Web.Tests.Controllers
+{
+    using System.Threading.Tasks;
+
+    using MyTested.AspNetCore.Mvc;
+
+    using UpSkill.Web.Areas.Admin.Company;
+    using UpSkill.Web.ViewModels.Company;
+
+    using Xunit;
+
+    public class CompaniesControllerTest
+    {
+        [Fact]
+        public void PostCreateShouldBeAllowedOnlyForPostRequestsAndAdminAuthorization()
+            => MyController<CompaniesController>
+            .Instance()
+            .Calling(c => c.Create(With.Default<CreateCompanyRequestModel>()).GetAwaiter().GetResult())
+            .ShouldHave()
+            .ActionAttributes(attributes => attributes
+            .RestrictingForAuthorizedRequests(withAllowedRoles: "Admin")
+            .RestrictingForHttpMethod(HttpMethod.Post));
+    }
+}
