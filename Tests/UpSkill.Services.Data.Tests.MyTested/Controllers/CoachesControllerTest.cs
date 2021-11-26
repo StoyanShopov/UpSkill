7d13b056edd31e3d73b060377de7b798ec11dd2d
spetrovne <sliper96@abv.bg>
2021-11-26 17:34:47 +0200

    Tested if CoachesController returns correct model

---
+++ Tests/UpSkill.Services.Data.Tests.MyTested/Controllers/CoachesControllerTest.cs
@@ -0,0 +1,26 @@
+﻿using MyTested.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UpSkill.Web.Controllers;
+using UpSkill.Web.ViewModels.Coach;
+using Xunit;
+
+namespace UpSkill.Services.Data.Tests.MyTested.Controllers
+{
+    public class CoachesControllerTest
+    {
+        [Fact]
+        public void GetAllShouldReturnAllCoaches()
+        {
+            MyController<CoachesController>
+                   .Instance()
+                   .Calling(c => c.GetAll())
+                   .ShouldReturn()
+                   .ResultOfType<IEnumerable<CoachListingModel>>();
+        }
+    }
+}
+
