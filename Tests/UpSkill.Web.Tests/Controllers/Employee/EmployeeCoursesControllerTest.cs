4450570b84bbd847ef683ba80869ee01d7c06404
ZdravkoBorisov <45686031+ZdravkoBorisov@users.noreply.github.com>
2021-11-29 17:29:01 +0200

    add tests

---
+++ Tests/UpSkill.Web.Tests/Controllers/Employee/EmployeeCoursesControllerTest.cs
@@ -0,0 +1,59 @@
+﻿namespace UpSkill.Web.Tests.Controllers.Employee
+{
+    using MyTested.AspNetCore.Mvc;
+    using System.Collections.Generic;
+    using System.Linq;
+    using UpSkill.Services.Data.Tests.Common;
+    using UpSkill.Web.Areas.Employee.Course;
+    using UpSkill.Web.ViewModels.Course;
+    using Xunit;
+
+    public class EmployeeCoursesControllerTest : TestWithData
+    {
+        [Fact]
+        public void GetAllShouldBeAllowedOnlyForGetRequests()
+        => MyController<CoursesController>
+           .Instance()
+            .Calling(c => c.GetAll())
+            .ShouldHave()
+            .ActionAttributes(attributes => attributes
+            .RestrictingForHttpMethod(HttpMethod.Get));
+
+        [Fact]
+        public void GetByIdShouldBeAllowedOnlyForGetRequests()
+       => MyController<CoursesController>
+          .Instance()
+           .Calling(c => c.GetByIdCourse(1))
+           .ShouldHave()
+           .ActionAttributes(attributes => attributes
+           .RestrictingForHttpMethod(HttpMethod.Get));
+
+        [Theory]
+        [InlineData(1)]
+        public void GetByIdShouldReturnCorrectDataWithValidModel(int id)
+        {
+            this.InitializeDatabase("test");
+
+            MyController<CoursesController>
+            .Instance()
+            .WithData(this.Database.Courses.ToList())
+            .Calling(c => c.GetByIdCourse(id))
+            .ShouldReturn()
+            .ResultOfType<DetailsViewModel>(result => result
+            .Passing(r => r.Id == id));
+        }
+
+        [Fact]
+        public void GetAllShouldReturnCorrectDataWithValidModel()
+        {
+            this.InitializeDatabase("test");
+
+            MyController<CoursesController>
+              .Instance()
+              .WithData(this.Database.Courses.ToList())
+              .Calling(c => c.GetAll())
+              .ShouldReturn()
+              .ResultOfType<IEnumerable<CoursesListingModel>>();
+        }
+    }
+}
---
+++ Tests/UpSkill.Web.Tests/Controllers/Owner/OwnerEmployeeControllerTest.cs
@@ -0,0 +1,56 @@
+﻿namespace UpSkill.Web.Tests.Controllers.Owner
+{
+    using MyTested.AspNetCore.Mvc;
+    using UpSkill.Services.Data.Tests.Common;
+    using UpSkill.Web.Areas.Owner.Employee;
+    using UpSkill.Web.ViewModels.Employee;
+    using Xunit;
+
+    public class OwnerEmployeeControllerTest : TestWithData
+    {
+        [Fact]
+        public void GetAllShouldBeAllowedOnlyForGetRequests()
+        {
+            MyController<EmployeeController>
+                .Instance()
+                .Calling(c => c.GetAll())
+                .ShouldHave()
+                .ActionAttributes(attributes => attributes
+                .RestrictingForHttpMethod(HttpMethod.Get));
+        }
+
+        [Fact]
+        public void CreateShouldBeAllowedOnlyForPostRequests()
+        {
+            MyController<EmployeeController>
+                .Instance()
+                .Calling(c => c.Create(With.Default<CreateEmployeeViewModel>()))
+                .ShouldHave()
+                .ActionAttributes(attributes => attributes
+                .RestrictingForHttpMethod(HttpMethod.Post));
+        }
+
+        [Fact]
+        public void DeleteShouldBeAllowedOnlyForDeleteRequests()
+           => MyController<EmployeeController>
+               .Instance()
+               .Calling(c => c.DeleteAsync("pitbull"))
+               .ShouldHave()
+               .ActionAttributes(attributes => attributes
+               .RestrictingForHttpMethod(HttpMethod.Delete));
+
+        [Fact]
+        public void GetAllCompanyEmployeesShouldBeAllowedOnlyForGetRequestes()
+            => MyController<EmployeeController>
+            .Instance()
+            .Calling(c => c.GetAllCompanyEmployees())
+            .ShouldHave()
+            .ActionAttributes(attributes => attributes
+            .RestrictingForHttpMethod(HttpMethod.Get));
+
+        [Fact]
+        public void DeleteShouldReturnSuccessfullyDeleted()
+            => MyController<EmployeeController>
+            .Instance()
+    }
+}
