41a6600f344ea4516f1f9b287f6725f78989b076
spetrovne <sliper96@abv.bg>
2021-12-13 16:26:53 +0200

    Updated Course/Coach details modal

--- src/Web/UpSkill.Web/ClientApp/src/services/adminDashboardService.js
+++ src/Web/UpSkill.Web/ClientApp/src/services/adminDashboardService.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 import {Base_URL} from '../utils/baseUrlConstant';
 
-const token = localStorage.getItem("token");
 
 export const adminDashboardGet = async () => {
+const token = localStorage.getItem("token");
+
   try {
     const resp = await axios.get(Base_URL + 'Admin/Dashboard', {headers: {"Authorization" : `Bearer ${token}`}});
     return resp.data
