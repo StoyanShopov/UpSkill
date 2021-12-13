af1eb53262a7d517811ab8d8a265443ab90c5c5d
spetrovne <sliper96@abv.bg>
2021-12-13 20:54:13 +0200

    Fixed Owner profile courses

--- src/Web/UpSkill.Web/ClientApp/src/components/MyProfile/CompanyOwnerViews/Courses/CoursesCatalog/CoursesCard/CoursesCard.js
+++ src/Web/UpSkill.Web/ClientApp/src/components/MyProfile/CompanyOwnerViews/Courses/CoursesCatalog/CoursesCard/CoursesCard.js
@@ -5,47 +5,61 @@ import "./OwnerCoursesCard.css";
 import GoogleLogo from "../../../../../../assets/img/courses/Image 2.png";
 
 function CourseCard(props) {
-    const { id, courseTitle, courseCoachFirstName, courseCoachLastName, courseFileFilePath, coursePrice, companyLogo, isDetailsOpen, getDetails  } = 
-    props.coursesDetails;
-  
-        return (
-            <div className="ownerCardContainer">
-                <div className="coursesImageWrapper"> 
-                    <div className="coursesImage" 
-                    /* onClick = {() => {
+  const {
+    id,
+    courseTitle,
+    courseCoachFirstName,
+    courseCoachLastName,
+    courseFileFilePath,
+    coursePrice,
+    companyLogo,
+    isDetailsOpen,
+    getDetails,
+  } = props.coursesDetails;
+
+  return (
+    <div className="ownerCardContainer">
+      <div className="coursesImageWrapper">
+        <div
+          className="coursesImage"
+          /* onClick = {() => {
                          isDetailsOpen(true);
                          getDetails({ id, courseTitle });
                         }} 
-                    */ 
-                    >
-                        <img src={ courseFileFilePath } alt="courses" style={{ width: 450, height: 248 }} />
-                    </div>
-                        <span className="courseImageTitle">{ courseTitle }</span>
-                </div>
-                <div className="ownerCardBody row">
-                    <div className="cardText col-md-5">
-                        <p id="course">{ courseTitle }</p>
-                    </div>
-                    
-                    <div className="col-md-5">
-                        <p id="name">{ courseCoachFirstName } { courseCoachLastName }</p>
-                    </div>
-            
-                    <div className="row">
-                        <div className="cardText col-md-6">
-                            <p id="price">{ coursePrice }€ per person</p>
-                        </div>
-                
-                        <div className="logo col-md-6">
-                            <img src={ GoogleLogo } alt="logo" />
-                        </div>
-                    </div>
-                </div>
-                <Button className="button row col-md-4">
-                    <p className="cardButtonText">Enroll</p>
-                </Button>
-            </div>
-        );
-    }
+                    */
+        >
+          <img
+            src={courseFileFilePath}
+            alt="courses"
+            style={{ width: 450, height: 248 }}
+          />
+        </div>
+        <span className="courseImageTitle">{courseTitle.split(" ")[0]}</span>
+      </div>
+      <div className="ownerCardBody row">
+        <div className="myprofile-course-cardText col-md-5">
+          <p id="myprofile-course">{courseTitle}</p>
+        </div>
+
+        <div className="col-md-5">
+          <p id="myprofile-name">
+            {courseCoachFirstName} {courseCoachLastName}
+          </p>
+        </div>
+
+        <div className="row">
+          <div className="myprofile-course-cardText col-md-6">
+            <p id="myprofile-price">{coursePrice}€ per person</p>
+          </div>
+
+          <div className="logo col-md-6">
+            <img src={GoogleLogo} alt="logo" />
+          </div>
+        </div>
+      </div>
+      {props.children}
+    </div>
+  );
+}
 
 export default CourseCard;
