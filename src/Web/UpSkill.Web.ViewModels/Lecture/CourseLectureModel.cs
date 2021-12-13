08b64495e907ae605f5749ab5be64168318bb065
Kiril <kiril24us@yahoo.com>
2021-12-13 18:29:52 +0200

    implement CourseDetails

---
+++ src/Web/UpSkill.Web.ViewModels/Lecture/CourseLectureModel.cs
@@ -0,0 +1,29 @@
+﻿namespace UpSkill.Web.ViewModels.Lecture
+{
+    using System.Collections.Generic;
+
+    using AutoMapper;
+
+    using UpSkill.Data.Models;
+    using UpSkill.Services.Mapping;
+    using UpSkill.Web.ViewModels.Lesson;
+
+    public class CourseLectureModel : IMapFrom<Lecture>, IHaveCustomMappings
+    {
+        public int Id { get; set; }
+
+        public string LectureName { get; set; }
+
+        public string LectureDescription { get; set; }
+
+        public IEnumerable<LectureLessonModel> LectureLessons { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<CourseLecture, CourseLectureModel>()
+               .ForMember(
+               l => l.Id,
+               l => l.MapFrom(l => l.LectureId));
+        }
+    }
+}
