c5294bad93023f312833b2072d4a0b12f1347c09
Chris Ivanov <65026817+ChrisIvanov@users.noreply.github.com>
2021-12-13 14:42:05 +0200

    Delete Web directory

--- Web/UpSkill.Web/ClientApp/node_modules/react-bootstrap/esm/ToastContext.d.ts
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-export interface ToastContextType {
-    onClose?: (e: Event) => void;
-}
-declare const ToastContext: React.Context<ToastContextType>;
-export default ToastContext;
--- Web/UpSkill.Web/ClientApp/node_modules/react-validation/src/main.js
+++
@@ -1,3 +0,0 @@
-export { default as form } from './hocs/form';
-export { default as control } from './hocs/control';
-export { default as button } from './hocs/button';
